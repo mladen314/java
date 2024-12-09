3b79ba5b67a6e15a65a46b1058cf942f5566f78a
mladen314 <mladen.jankovic.314@gmail.com>
2024-12-09 21:53:40 +0100

    a

--- Test/src/java241009/Main.java
+++ Test/src/java241009/Main.java
@@ -3,8 +3,33 @@ package java241009;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Ovo radi");
+        /*
+		Ucenik ucenik1 = new Ucenik();
+        ucenik1.ime = "Ana";
+        ucenik1.razred = 3;
+
+        Ucenik ucenik2 = new Ucenik();
+        ucenik2.ime = "Marko";
+        ucenik2.razred = 3;
+*/
+        
+        Ucenik ucenik1 = new Ucenik("Ana", 3);
+        Ucenik ucenik2 = new Ucenik("Marko", 3);
+        ucenik1.predstaviSe();
+        ucenik2.predstaviSe();	
 	}
 
+}
+class Ucenik {
+    public Ucenik(String ime, int razred) {
+		this.ime = ime;
+		this.razred = razred;
+	}
+
+	String ime;
+    int razred;
+
+    public void predstaviSe() {
+        System.out.println("Zdravo, ja sam " + ime + " i pohađam " + razred + " razred.");
+    }
 }
